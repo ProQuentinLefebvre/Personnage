a112cbb9060f8277dfa48e4ca6d3efda8dc7f1b8
ProQuentinLefebvre <98103352+ProQuentinLefebvre@users.noreply.github.com>
2022-02-08 15:13:14 +0100

    Update Personnage.java

--- Personnage.java
+++ Personnage.java
@@ -1,25 +1,58 @@
 package Personnage;
+
 public class Personnage {
-//le constructeur 
-public Personnage (String name, int life){
-	this.nom = name;
-	this.vie = life;
+	//public String honte_nom;
+	// les attributs
+	private String nom;
+	private int vie;
+
+	// le constructeur
+	public Personnage(String name, int life) {
+		//this.nom = name;
+		this.setNom(name);
+		this.vie = life;
+	}
+
+	// accesseur
+	public String leNom() {
+		return this.nom;
+	}
+
+	// accesseur
+	public String getNom() {
+		return nom;
+	}
+
+	// mutateur
+	public void setNom(String nom) {
+		if(nom.length()>15)
+			nom = nom.substring(0, 15);
+		this.nom = nom;
+	}
+
+	public int laVie() {
+		return this.vie;
+	}
+
+	public void setVie(int vie) {
+		this.vie=vie;
+	}
+
+	public boolean degat(int nb){
+		//setVie(laVie()-nb);
+		setVie(this.vie-nb);
+		if(laVie()>0)
+			return true;
+		return false;
+	}
+
+	public int attack(){
+		return 10;
+	}
+	public int attack(int pt){
+		return pt;
+	}
+
 }
 
-//accesseur 
-public String leNom() {
- 	 return this.nom;
-}
-public int laVie() {
- 	 return this.vie;
-}  
-//les attributs
-private String nom;
-private int vie;
-}
-
-//mutateur 
-public void setnom(String pnom) {
-	
-}
 
