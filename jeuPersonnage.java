d031f88a740b6a16519200fd37f9630d7f18f047
ProQuentinLefebvre <98103352+ProQuentinLefebvre@users.noreply.github.com>
2022-02-08 15:12:15 +0100

    Update jeuPersonnage.java

--- jeuPersonnage.java
+++ jeuPersonnage.java
@@ -1,11 +1,21 @@
-import Personnage.*;
+import Personnage.Personnage;
 
-	public class jeuPersonnage {
-		public static void main (String args[]) {
-			Personnage UnSorcier = new Personnage ("Quentin", 200);
-			System.out.println("infos sur le Personnage" + UnSorcier.leNom());
+public class jeuPersonnage {
+    public static void main(String[] args) throws Exception {
+      
+        Personnage UnSorcier = new Personnage("Quentin", 200);
+        Personnage Unhumain = new Personnage("Jacques", 200);
+        System.out.println("le personnage est -->" + UnSorcier.leNom());
+        System.out.println("le personnage est -->" + Unhumain.leNom());
+    
+        System.out.println("infos sur le Personnage ---->" + UnSorcier.leNom());
+        System.out.println("Je suis en vie: (attack "+Unhumain.attack()+")" + UnSorcier.degat(Unhumain.attack()) +" j'ai " + UnSorcier.laVie());
+        System.out.println("Je suis en vie: (attack "+Unhumain.attack(40)+")" + UnSorcier.degat(Unhumain.attack(40)) +" j'ai " + UnSorcier.laVie());
+        if(UnSorcier.laVie()>0)
+            System.out.println("ouf je suis en vie");
+        else
+            System.out.println("argggg");
+        
 
-			Personnage Unhumain = new Personnage ("Jacques", 200);
-			System.out.println("le personnage est -->" + Unhumain.leNom());
-			}
-}			
+    }
+}
